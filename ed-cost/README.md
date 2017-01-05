ca25770f9099215d0b4eb2c74e294aba0c55103d
mkfreeman <michael.k.freeman@gmail.com>
2017-01-05 15:05:28 -0800

    Added complete answers

--- ed-cost/README.md
+++ ed-cost/README.md
@@ -1,3 +1,12 @@
 # Educational cost
 
 The data for this exercise comes from the U.S. Department of Education's [College Cost](https://collegecost.ed.gov/catc/Default.aspx) website. More specifically, it is the `TuitionChange` tab of the Excel document downloaded [here](https://collegecost.ed.gov/catc/resources/CATClists2014.xlsx).
+
+In this exercise, you'll answer the following questions (as well as your own):
+
+- What is the average annual tuition by sector in 2014?
+- How did the cost of UW rank against other Washington schools?
+- Which *sector* had the largest average change in tuition?
+- Which state has the most higher-ed institutions?
+
+The `analysis.R` is a partially completed version of this exercise that demonstrates how to begin working the the data. The answers are in the `analysis-complete.R` file.
--- ed-cost/analysis-complete.R
+++ ed-cost/analysis-complete.R
@@ -47,22 +47,66 @@ most.expensive <- ed.data %>%
       select(Name.of.institution)
 
 # What was the least expensive school in 2014?
-
+least.expensive <- ed.data %>% 
+                    filter(tuition.2014 == min(tuition.2014)) %>%  
+                    select(Name.of.institution)
 
 #### What is the average annual tuition by sector in 2014? ####
-
+by.sector <- ed.data %>% 
+              group_by(Sector.name) %>% 
+              summarize(avg.tuition = mean(tuition.2014, na.rm = T))
 
 # Show a `barplot of the cost by sector
-
+barplot(by.sector$avg.tuition, 
+          names.arg = by.sector$Sector.name, # specify labels
+          horiz = TRUE, # specify horizontal plot
+          las=1, # Argument for setting label angles
+          cex.names = .5 # Decrease label size
+        )
 
 #### How did the cost of UW rank against other Washington schools? ####
 
+# Filter down to Washington schools, then compute the rank
+wa.data <- ed.data %>% filter(State == 'WA') %>%
+           mutate(wa.rank = rank(-tuition.2014))
 
 #### Which *sector* had the largest average change in tuition? ####
-
+sector.average <- ed.data %>% 
+  mutate(change = tuition.2014 - tuition.2012) %>% 
+  group_by(Sector, Sector.name) %>% 
+  summarize(avg.change = mean(change, na.rm = TRUE))
 
 ##### Which state has the most higher-ed institutions? #####
-
+by.state <- ed.data %>% 
+            group_by(State) %>% 
+            summarize(num.schools = n())
 
 ##### Bonus: Map the number of schools using plotly #####
 # Hint: See https://plot.ly/r/choropleth-maps/
+
+# Store map in a variable
+simple.map <- plot_geo(by.state, locationmode = 'USA-states') %>% 
+        add_trace(
+          z = ~num.schools, color = ~num.schools, locations = ~State
+        )
+
+# Show the map
+simple.map
+
+# Set geography options for map
+g <- list(
+  scope = 'usa',
+  projection = list(type = 'albers usa'),
+  showlakes = TRUE,
+  lakecolor = toRGB('white')
+)
+
+# Pass simple map into a layout function
+map.with.layout <- simple.map %>% 
+        layout(
+          title = 'Number of Schools per State',
+          geo = g
+        )
+
+# Show map with a better layout
+map.with.layout
